93c387a375493f86a1257824c5a13904fa464e5e
F14 <f14commander@gmail.com>
2020-12-27 10:43:18 +0300

    add GitHub

--- wsearch.py
+++ wsearch.py
@@ -1,14 +1,17 @@
 from webbrowser import open
 print("")
-print("Youtube (1) Twitter (2)")
+print("Youtube (1) Twitter (2) GitHub(3)")
 print("")
-program = input("Please Enter What you want to search : ")
+web = input("Please Enter What you want to search : ")
 print("-" * 60)
-if program =="1" :
+if web =="1" :
     search = input("Please Enter What you want to search : ")
     open('https://www.youtube.com/results?search_query=' + search)
-elif program =="2" :
+elif web =="2" :
     search = input("Please Enter What you want to search : ")
     open('https://twitter.com/search?q=' + search)
+elif web =="2":
+    search = input("Please Enter What you want to search : ")
+    open('https://github.com/search?q=' + search)
 else:
-    exit("Error... Please Enter [1] or [2]")
+    exit("Error... Please Enter [1] or [2] or [3]")
