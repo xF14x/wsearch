570dbe8166d5d0153e21d97f5082e1d88de3a3f4
xF14x <>
2021-01-14 23:35:24 +0300

    modified:   wsearch.py 	add Sultan Code And Fix Github elif

--- wsearch.py
+++ wsearch.py
@@ -1,7 +1,15 @@
 from webbrowser import open
 print("")
-print("Youtube (1) Twitter (2) GitHub(3)")
+print("""
+Programmit By Suliman | Twitter : F14Commander
+Youtube (1) Twitter (2) GitHub(3)
+
+Programmit By Sultan | Twitter | sultaan17_
+Google translate(4) Shodan(5)
+
+""")
 print("")
+# By Suliman
 web = input("Please Enter What you want to search : ")
 print("-" * 60)
 if web =="1" :
@@ -10,8 +18,15 @@ if web =="1" :
 elif web =="2" :
     search = input("Please Enter What you want to search : ")
     open('https://twitter.com/search?q=' + search)
-elif web =="2":
+elif web =="3":
     search = input("Please Enter What you want to search : ")
     open('https://github.com/search?q=' + search)
+    # By Sultan
+elif web == "4":
+    search = input("Please Enter What you want to search : ")
+    open('https://translate.google.com/?sl=en&tl=ar&text='+ search +'&op=translate')
+elif web == "5":
+    search = input("Please Enter What you want to search : ")
+    open("https://www.shodan.io/search?query="+ search )
 else:
-    exit("Error... Please Enter [1] or [2] or [3]")
+    exit("Error... Please Enter [1] or [2] or [3] or [4] or [5]")
